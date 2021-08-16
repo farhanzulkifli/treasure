681623721ad33157a5bfe860281e6232db2ca243
Darylaw93 <darylaw93@gmail.com>
2021-08-16 22:48:36 +0800

    nav bar done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,16 +8,15 @@ export default function NavBar() {
         <div className="container">
         <div className="container2">
         <div className="navBar">
-            Nav Bar on the left side
-            <div>(IMG)</div>
-            <p>Hello User,</p>
-            <ul >
-            <Link to = {`${url}/dashboard`} className="Link"><li>Dashboard</li></Link>
-            <Link to = {`${url}/realmap`} className="Link"><li>Map</li></Link>
-            <Link to = {`${url}/tweets`} className="Link"><li>Tweets</li></Link>
-            <Link to = {`${url}/messages`} className="Link"><li>Messages</li></Link>
-            <Link to = {`${url}/aboutus`} className="Link"><li>About Us</li></Link>
-            <Link to = "/" className="Link"><li>Log Out</li></Link>
+            <div className="ProfilePic"><img className="pic" src="https://img.icons8.com/ios-filled/50/000000/indiana-jones.png"/></div>
+            <h1 className="ProfilePic">Hello User!</h1>
+            <ul>
+            <Link to = {`${url}/dashboard`} className="Link"><li><img src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-vitaly-gorbachev/452/external-dashboard-blogger-vitaliy-gorbachev-lineal-vitaly-gorbachev.png"/>Dashboard</li></Link>
+            <Link to = {`${url}/realmap`} className="Link"><li><img src="https://img.icons8.com/material-outlined/50/000000/map--v1.png"/>Map</li></Link>
+            <Link to = {`${url}/tweets`} className="Link"><li><img src="https://img.icons8.com/ios-glyphs/30/000000/stack-of-tweets.png"/>Tweets</li></Link>
+            <Link to = {`${url}/messages`} className="Link"><li><img src="https://img.icons8.com/ios-glyphs/30/000000/message-group.png"/>Messages</li></Link>
+            <Link to = {`${url}/aboutus`} className="Link"><li><img src="https://img.icons8.com/ios/50/000000/indiana-jones.png"/>About Us</li></Link>
+            <Link to = "/" className="Link"><li><img src="https://img.icons8.com/ios-glyphs/30/000000/logout-rounded-left.png"/>Log Out</li></Link>
             </ul>
         </div>
         </div>
