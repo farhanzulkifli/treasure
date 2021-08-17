b89eaf632e170e90d11a2b59df75c089cc9bbdcb
Darylaw93 <darylaw93@gmail.com>
2021-08-17 17:18:11 +0800

    token completed

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import axios from "axios";
 
 const url = "https://quiet-taiga-82025.herokuapp.com";
 
 export default function Login() {
-
+let history = useHistory();
 
 const handleLogin = (event) => {
     event.preventDefault()
-    const username = (event.target.usernameusername).toLowerCase()
+    const username = (event.target.username.value).toLowerCase()
     
     axios.post(`${url}/user/login/`, {
         username: username,
-        password: event.target.password
+        password: event.target.password.value
     })
     .then(function (res) {
-        console.log(res)
+        localStorage.setItem('access_token', res.data.token)
+        localStorage.setItem('refresh_token', res.data.refresh)
+        console.log(localStorage)
+        history.push("/home/realmap")
     })
     .catch(function (err) {
         console.log(err);
